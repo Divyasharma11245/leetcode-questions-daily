b36f7cf8fda8834aa411740a5c9bbce1f5b451d3
Divyasharma11245 <divyasharma11245@gmail.com>
2025-11-25 15:10:23 +0530

    Time: 4 ms (53.34%), Space: 45.1 MB (92.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        //approach 1 - using map
-        HashMap <Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        }
-
-        Set<Integer> set = map.keySet();
-
-        for(int ele: set){
-            if(map.get(ele)==1){
-                return ele;
+        //approach 2 - using bits and counting the number of set bits
+        int ans = 0;
+        for(int i = 0; i<32; i++){
+            int count = 0;
+            for(int  j= 0; j<nums.length; j++){
+                if((nums[j]&(1<<i))!=0){
+                    count++;
+                }
+            }
+            if(count%3==1){
+                ans = ans|(1<<i);
             }
         }
-        return -1;
+        return ans;
     }
 }
