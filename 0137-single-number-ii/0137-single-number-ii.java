dca32205bd883f8e753ea12deea95093e44c3114
Divyasharma11245 <divyasharma11245@gmail.com>
2025-11-25 15:18:04 +0530

    Time: 8 ms (8.88%), Space: 45.7 MB (38.8%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,18 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        //approach 2 - using bits and counting the number of set bits
-        int ans = 0;
-        for(int i = 0; i<32; i++){
-            int count = 0;
-            for(int  j= 0; j<nums.length; j++){
-                if((nums[j]&(1<<i))!=0){
-                    count++;
-                }
-            }
-            if(count%3==1){
-                ans = ans|(1<<i);
-            }
+        //approach 3 - using simple maths
+        Arrays.sort(nums);
+       for(int i = 1; i<nums.length; i=i+3){
+        if(nums[i]!=nums[i-1]){
+            return nums[i-1];
         }
-        return ans;
+       }
+       return nums[nums.length-1];
     }
 }
