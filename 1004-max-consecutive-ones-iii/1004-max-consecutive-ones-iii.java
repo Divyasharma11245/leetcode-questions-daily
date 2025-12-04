0fae5fafc9b01c81469cbd6ecd74ad89106175a4
Divyasharma11245 <divyasharma11245@gmail.com>
2025-12-04 14:59:42 +0530

    Time: 4 ms (43.17%), Space: 51.9 MB (94.93%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -11,7 +11,7 @@ class Solution {
                 zeroes++;
             }
 
-            while(zeroes>k) {
+            if(zeroes>k) {
             if(nums[l]==0) zeroes--;
             l++;
         }
