9a71e172ad57d91a5a4e816ffbb5fe07ec242c38
Divyasharma11245 <divyasharma11245@gmail.com>
2026-01-27 14:20:10 +0530

    Time: 11 ms (94.67%), Space: 47.8 MB (42.47%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,15 +6,25 @@ class Solution {
             map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
 
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        List<Integer>bucket[] = new List[nums.length+1];
         
-        Collections.sort(list, (a, b) -> b.getValue() - a.getValue());
-
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = list.get(i).getKey();
+        for(Integer key: map.keySet()){
+            int freq = map.get(key);
+            if(bucket[freq]==null){
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
         }
 
+        int result[] = new int[k];
+        int counter = 0;
+        for(int i = bucket.length-1; i>=0&&counter<k; i--){
+            if(bucket[i]!=null){
+                for(Integer ans: bucket[i]){
+                    result[counter++] = ans;
+                }
+            }
+        }
         return result;
    
     }
