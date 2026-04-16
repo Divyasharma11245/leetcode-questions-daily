25a478f102aafac196123f9de5b98ba8cb804910
Divyasharma11245 <divyasharma11245@gmail.com>
2026-04-17 04:23:38 +0530

    Time: 106 ms (40.73%), Space: 47.4 MB (6.25%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,13 +1,22 @@
 class Solution {
-    private int solve(int[] nums, int i, int currSum, int target){
+    private int solve(int[] nums, int i, int currSum, int target, HashMap<String, Integer> map){
         if(i==nums.length) return currSum == target ? 1 : 0;
 
-        int add = solve(nums, i+1, currSum+nums[i], target);
-        int sub = solve(nums, i+1, currSum-nums[i], target);
+        String key = i + "-" + currSum;
 
-        return add+sub;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+
+        int add = solve(nums, i+1, currSum+nums[i], target, map);
+        int sub = solve(nums, i+1, currSum-nums[i], target, map);
+
+        map.put(key, add + sub);
+        return map.get(key);
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums, 0, 0, target);
+        HashMap<String, Integer> map = new HashMap<>();
+        return solve(nums, 0, 0, target, map);
+        
     }
 }
