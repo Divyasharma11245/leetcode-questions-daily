42c50d4fbbbd6ea2a76e8c80058362c8404a8349
Divyasharma11245 <divyasharma11245@gmail.com>
2026-04-21 10:03:39 +0530

    Time: 11 ms (59.87%), Space: 56.5 MB (48.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,23 +1,25 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int dp[][] = new int[n + 1][amount + 1];
-
-        for (int i = 0; i < dp.length; i++) {
+        int dp[][] = new int[n+1][amount+1];
+        for(int i =0; i<=n; i++){
             dp[i][0] = 1;
         }
-        for (int j = 0; j < dp[0].length; j++) {
+        for(int j =1; j<=amount; j++){
             dp[0][j] = 0;
         }
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < amount + 1; j++) {
-                if (coins[i - 1] <= j) { // valid
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j]; // total number of ways
-                } else { // invalid
-                    dp[i][j] = dp[i - 1][j];
+        for(int i =1; i<=n; i++){
+            for(int j =1; j<=amount; j++){
+                if(coins[i-1]<=j){ //valid
+                    int take = dp[i][j-coins[i-1]];
+                    int notTake = dp[i-1][j];
+                    dp[i][j] = take+notTake;
+                }else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
+
         }
         return dp[n][amount];
     }
