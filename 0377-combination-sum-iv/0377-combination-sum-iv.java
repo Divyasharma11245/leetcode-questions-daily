5c89f245f843df867dfc04cc516e76ef08a34dfa
Divyasharma11245 <divyasharma11245@gmail.com>
2026-04-21 10:33:38 +0530

    Time: 2 ms (35.15%), Space: 42.7 MB (64.42%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,30 +1,16 @@
 class Solution {
-    private int solve(int[] nums, int target, int idx, int dp[][]){
-        if(target==0){
-            return 1;
-        }
-        if(idx>=nums.length){
-            return 0;
-        }
-        if(dp[idx][target]!=-1){
-            return dp[idx][target];
-        }
-        int ways = 0;
+    public int combinationSum4(int[] nums, int target) {
+        int dp[] = new int[target+1];
+        dp[0] = 1;
 
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] <= target){
-                ways += solve(nums, target - nums[i], 0, dp);
+        for(int j = 1; j <= target; j++){
+            for(int i = 0; i < nums.length; i++){
+                if(nums[i] <= j){
+                    dp[j] += dp[j - nums[i]];
+                }
             }
         }
 
-        return dp[idx][target] = ways;
-    }
-    public int combinationSum4(int[] nums, int target) {
-        int n = nums.length;
-        int dp[][] = new int[n][target+1];
-        for(int i =0; i<n; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return solve(nums, target, 0, dp);
+        return dp[target];
     }
 }
