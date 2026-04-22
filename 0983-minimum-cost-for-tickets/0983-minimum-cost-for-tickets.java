48cfc1340a0835a7044bf32960208f7216830206
Divyasharma11245 <divyasharma11245@gmail.com>
2026-04-23 04:06:58 +0530

    Time: 2 ms (44.35%), Space: 43.6 MB (27.98%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,33 +1,25 @@
 class Solution {
-    private int solve(int[] days, int[] costs, int n, int i, int dp[]){
-        if(i>=days.length){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int cost1 = costs[0]+solve(days, costs, n, i+1, dp);
-        
-        int j = i;
-        int maxDays7 = days[i]+7;
-        while(j<n&&days[j]<maxDays7){
-            j++;
-        }
-        int cost2 = costs[1]+solve(days, costs, n, j, dp);
-
-        int k = i;
-        int maxDays30 = days[i]+30;
-        while(k<n&&days[k]<maxDays30){
-            k++;
-        }
-        int cost3 = costs[2]+solve(days, costs, n, k, dp);
-
-        return dp[i] = Math.min(cost1, Math.min(cost2, cost3));
-    }
     public int mincostTickets(int[] days, int[] costs) {
-        int n = days.length;
-        int dp[] = new int[366];
-        Arrays.fill(dp, -1);
-        return solve(days, costs, n, 0, dp);
+        int maxDay = days[days.length-1];
+        int dp[] = new int[maxDay+1];
+
+        dp[0] = 0;
+        HashSet<Integer> st = new HashSet<>();
+        for(int i =0; i<days.length; i++){
+            st.add(days[i]);
+        }
+
+        for(int i =1; i<=maxDay; i++){
+            if(!st.contains(i)){
+                dp[i] = dp[i-1];
+                continue;
+            }
+            int cost1 = costs[0]+dp[Math.max(i-1, 0)];
+            int cost2 = costs[1]+dp[Math.max(i-7, 0)];
+            int cost3 = costs[2]+dp[Math.max(i-30, 0)];
+            dp[i] = Math.min(cost1, Math.min(cost2, cost3));
+        }
+
+        return dp[maxDay];
     }
 }
