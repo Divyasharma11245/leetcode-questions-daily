6935187d08397049d7d9020f23f83db0ca4c74ec
Divyasharma11245 <divyasharma11245@gmail.com>
2026-05-17 03:32:19 +0530

    Time: 0 ms (100%), Space: 43.9 MB (42.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,16 +1,14 @@
 class Solution {
-    private boolean search(int[][] matrix, int target, int row){
+    private boolean binarySearch(int s, int e, int row, int matrix[][], int target){
         int n = matrix[0].length;
-        int st = 0;
-        int end = n-1;
-        while(st<=end){
-            int mid = st+(end-st)/2;
+        while(s<=e){
+            int mid = s+(e-s)/2;
             if(target==matrix[row][mid]){
                 return true;
-            }else if(target>matrix[row][mid]){
-                st=mid+1;;
+            }else if(target<matrix[row][mid]){
+                e = mid-1;
             }else{
-                end = mid-1;
+                s = mid+1;
             }
         }
         return false;
@@ -18,16 +16,16 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int startRow = 0;
-        int endRow = m-1;
-        while(startRow<=endRow){
-            int mid = startRow+(endRow-startRow)/2;
-            if(target>=matrix[mid][0]&&target<=matrix[mid][n-1]){
-                return search(matrix, target, mid);
-            }else if(target>=matrix[mid][n-1]){
-                startRow = mid+1;
+        int sr = 0;
+        int er = m-1;
+        while(sr<=er){
+            int midR = sr+(er-sr)/2;
+            if(target>=matrix[midR][0]&&target<=matrix[midR][n-1]){
+                return binarySearch(0, n-1, midR, matrix, target);
+            }else if(target<matrix[midR][0]){
+                er=midR-1;
             }else{
-                endRow = mid-1;
+                sr=midR+1;
             }
         }
         return false;
