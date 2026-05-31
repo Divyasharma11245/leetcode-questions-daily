55548e5a1096b6d037feaea0f64985680dfe013e
Divyasharma11245 <divyasharma11245@gmail.com>
2026-05-31 18:43:27 +0530

    Time: 0 ms (100%), Space: 46.4 MB (34.78%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -9,49 +9,26 @@
  * }
  */
 class Solution {
-    private ListNode findKthNode(ListNode temp, int k){
-        k-=1;
-        while(temp!=null&&k>0){
-            k--;
-            temp = temp.next;
-        }
-        return temp;
-    }
-    private ListNode reverse(ListNode head){
-        ListNode prev = null;
-        ListNode curr = head;
-        ListNode next;
-        while(curr!=null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    }
     public ListNode reverseKGroup(ListNode head, int k) {
         ListNode temp = head;
-        ListNode nextNode;
-        ListNode prev = null;
-
-        while(temp!=null){
-            ListNode kthNode = findKthNode(temp, k);
-            if(kthNode == null){
-                if(prev!=null) prev.next = temp;
-                break;
+        int count = 0;
+        while(count<k){
+            if(temp==null){
+                return head;
             }
-            nextNode = kthNode.next;
-            kthNode.next = null;
-             ListNode reversed = reverse(temp);
-
-            if(temp==head){
-                head=reversed;
-            }else{
-                prev.next = reversed;
-            }
-            prev = temp;
-            temp = nextNode;
+            temp = temp.next;
+            count++;
         }
-        return head;
+        ListNode prev = reverseKGroup(temp, k);
+        temp = head;
+        count = 0;
+        while(count<k){
+            ListNode next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+            count++;
+        }
+        return prev;
     }
 }
