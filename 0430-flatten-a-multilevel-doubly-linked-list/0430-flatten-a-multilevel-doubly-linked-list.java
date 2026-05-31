cdf235ce1ffc156cd350204f73252860066181cd
Divyasharma11245 <divyasharma11245@gmail.com>
2026-05-31 16:59:30 +0530

    Time: 1 ms (41.18%), Space: 43.3 MB (86.77%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -10,8 +10,9 @@ class Node {
 
 class Solution {
     public Node flatten(Node head) {
-        if(head==null) return head;
-
+        if(head==null){
+            return head;
+        }
         Node curr = head;
         while(curr!=null){
             if(curr.child!=null){
@@ -19,17 +20,15 @@ class Solution {
                 curr.next = flatten(curr.child);
                 curr.next.prev = curr;
                 curr.child = null;
-
                 while(curr.next!=null){
                     curr = curr.next;
                 }
-
                 if(next!=null){
                     curr.next = next;
                     next.prev = curr;
                 }
             }
-            curr = curr.next;
+            curr=curr.next;
         }
         return head;
     }
