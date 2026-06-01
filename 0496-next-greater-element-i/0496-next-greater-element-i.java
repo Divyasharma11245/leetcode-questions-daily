53024a285ec58911bdceb4c1075d10f91c4e3605
Divyasharma11245 <divyasharma11245@gmail.com>
2026-06-02 05:29:05 +0530

    Time: 3 ms (93.49%), Space: 45.3 MB (35.92%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,37 +1,24 @@
 class Solution {
-    private ArrayList<Integer> findnextGreater(int[] arr) {
-        int n = arr.length;
-        Stack<Integer> s = new Stack<>();
-        ArrayList<Integer> nextGreater = new ArrayList<>();
-        for(int j = 0; j<n; j++) nextGreater.add(0);
-        for(int i = n-1; i>=0; i--){
-            while(!s.isEmpty()&&arr[s.peek()]<=arr[i]) {
-                s.pop();
-            }
-            
-            if(s.isEmpty()){
-                nextGreater.set(i, -1);
-            }else{
-                nextGreater.set(i, arr[s.peek()]);
-            }
-            
-            s.push(i);
-        }
-        return nextGreater;
-    }
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-       //phle saare next greater ko index ke sath hashmap me store krle
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> nextGreater = findnextGreater(nums2);
-        for(int i = 0; i<nums2.length; i++){
-            map.put(nums2[i], nextGreater.get(i));
+        Stack<Integer> s = new Stack<>();
+        HashMap<Integer, Integer> map= new HashMap<>();
+        int n = nums2.length;
+        for(int i = n-1; i>=0; i--){
+        while(!s.isEmpty()&&s.peek()<=nums2[i]){
+            s.pop();
+        }
+        if(s.isEmpty()) map.put(nums2[i], -1);
+        else map.put(nums2[i], s.peek());
+        s.push(nums2[i]);
         }
 
-        int n = nums1.length;
-        int ans[] = new int[n];
-        for(int i = 0; i<n; i++){
-                 ans[i] = map.get(nums1[i]);
-                }
-                return ans;
-            }
+
+        int ans[] = new int[nums1.length];
+        for(int i = 0; i<nums1.length; i++){
+            ans[i] = map.get(nums1[i]);
         }
+
+        return ans;
+
+    }
+}
