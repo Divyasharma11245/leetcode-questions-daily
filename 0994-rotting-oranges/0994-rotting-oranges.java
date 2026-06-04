c8cdfd1d25819e569e732b2f239b0db9d8d095bc
Divyasharma11245 <divyasharma11245@gmail.com>
2026-06-04 17:37:32 +0530

    Time: 2 ms (90.91%), Space: 44.4 MB (24.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -10,51 +10,48 @@ class Solution {
         }
     }
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
         Queue<Pair> q = new LinkedList<>();
-        boolean vis[][] = new boolean[n][m];
-
-        for(int i =0; i<n; i++){
-            for(int j = 0; j<m; j++){
+        int m = grid.length;
+        int n =grid[0].length;
+        boolean vis[][] = new boolean[m][n];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
                 if(grid[i][j]==2){
-                    q.add(new Pair(i, j, 0));
+                    q.add(new Pair(i,j, 0));
                 }
             }
         }
-
         int maxTime = 0;
         while(!q.isEmpty()){
             Pair curr = q.poll();
             int i = curr.i;
-            int j = curr.j;
+            int j = curr.j; 
             int time = curr.time;
-
             maxTime = Math.max(maxTime, time);
-            if(i-1>=0&& grid[i-1][j]==1&&!vis[i-1][j]){
-                grid[i-1][j] = 2;
+            
+            if(i-1>=0&&!vis[i-1][j]&&grid[i-1][j]==1){
                 vis[i-1][j] = true;
-                q.add(new Pair(i-1, j, time+1));
+                grid[i-1][j] = 2;
+                q.add(new Pair(i-1,j,time+1));
             }
-            if(j-1>=0&& grid[i][j-1]==1&&!vis[i][j-1]){
-                grid[i][j-1] = 2;
+            if(i+1<m&&!vis[i+1][j]&&grid[i+1][j]==1){
+                vis[i+1][j] = true;
+                grid[i+1][j] = 2;
+                q.add(new Pair(i+1,j,time+1));
+            }
+            if(j-1>=0&&!vis[i][j-1]&&grid[i][j-1]==1){
                 vis[i][j-1] = true;
+                grid[i][j-1] = 2;
                 q.add(new Pair(i, j-1, time+1));
             }
-            if(i+1<n&& grid[i+1][j]==1&&!vis[i+1][j]){
-                grid[i+1][j] = 2;
-                vis[i+1][j] = true;
-                q.add(new Pair(i+1, j, time+1));
-            }
-            if(j+1<m&& grid[i][j+1]==1&&!vis[i][j+1]){
-                grid[i][j+1] = 2;
+            if(j+1<n&&!vis[i][j+1]&&grid[i][j+1]==1){
                 vis[i][j+1] = true;
+                grid[i][j+1] = 2;
                 q.add(new Pair(i, j+1, time+1));
             }
         }
-
-        for(int i =0; i<n; i++){
-            for(int j = 0; j<m; j++){
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
                 if(grid[i][j]==1){
                     return -1;
                 }
@@ -62,5 +59,4 @@ class Solution {
         }
         return maxTime;
     }
-
 }
