0dbbf699ca9346b682ce462fea565269ddff9eba
Divyasharma11245 <divyasharma11245@gmail.com>
2026-06-22 17:40:20 +0530

    Time: 39 ms (22.71%), Space: 86.6 MB (18.2%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,39 +1,30 @@
 class Solution {
-    private void heapify(int nums[], int i, int size){
+    private void heapify(int[] arr, int i, int size){
         int left = 2*i+1;
         int right = 2*i+2;
         int maxIdx = i;
-        if(left<size&&nums[left]>nums[maxIdx]){
-            maxIdx = left;
-        }
-        if(right<size&&nums[right]>nums[maxIdx]){
-            maxIdx = right;
-        }
-
+        if(left<size&&arr[left]>arr[maxIdx]) maxIdx = left;
+        if(right<size&&arr[right]>arr[maxIdx]) maxIdx = right;
         if(maxIdx!=i){
-            int temp = nums[i];
-            nums[i] = nums[maxIdx];
-            nums[maxIdx] = temp;
+            int temp = arr[i];
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
 
-            heapify(nums, maxIdx, size);
+            heapify(arr, maxIdx, size);
         }
     }
-    private void heapSort(int nums[]){
+    public int[] sortArray(int[] nums) {
         int n = nums.length;
         for(int i = n/2; i>=0; i--){
             heapify(nums, i, n);
         }
 
-        for(int i = n-1; i>0; i--){
-            //swap
-            int temp = nums[i];
-            nums[i] = nums[0];
-            nums[0] = temp;
+        for(int i = n-1; i>=0; i--){
+            int temp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = temp;
             heapify(nums, 0, i);
         }
-    }
-    public int[] sortArray(int[] nums) {
-        heapSort(nums);
         return nums;
     }
 }
