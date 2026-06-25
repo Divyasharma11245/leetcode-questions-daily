da3f16da4cb35dcb69a4d3b192ff2682049f4f02
Divyasharma11245 <divyasharma11245@gmail.com>
2026-06-25 15:34:31 +0530

    Time: 4 ms (94.93%), Space: 46.4 MB (97.32%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,54 +1,59 @@
 class Solution {
-     private boolean dfsUtil(ArrayList<Integer> graph[], boolean vis[], boolean pathVis[], Stack<Integer> s, int curr){
-        vis[curr]  = true;
-        pathVis[curr] = true;
-        
-        for(int i = 0; i<graph[curr].size(); i++){
-            int neigh = graph[curr].get(i);
+     private boolean isCycle(ArrayList<ArrayList<Integer>> adj, int curr, boolean[] vis, boolean[] st){
+        vis[curr] = true;
+        st[curr] = true;
+        for(int neigh: adj.get(curr)){
             if(!vis[neigh]){
-                if(dfsUtil(graph, vis, pathVis, s, neigh)){
-                    return true;
-                }
-            }else if (pathVis[neigh]) {
-                return true;
-            }
+                 if(isCycle(adj, neigh, vis, st)) return true;
+            }else if(st[neigh]) return true;
         }
-       pathVis[curr] = false;
-        s.push(curr);
+        st[curr] = false;
         return false;
     }
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int V = numCourses;
-        ArrayList<Integer> graph[] = new ArrayList[V];
-        
-        for(int i = 0; i<V; i++){
-            graph[i] = new ArrayList<>();
+
+    private void topSort(ArrayList<ArrayList<Integer>> adj, int curr, boolean[] vis, Stack<Integer> st){
+        vis[curr] = true;
+        for(int neigh: adj.get(curr)){
+            if(!vis[neigh]){
+                topSort(adj, neigh, vis, st);
+            }
         }
-        
-        for(int edge[]: prerequisites){
+        st.push(curr);
+    }
+    public int[] findOrder(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int edge[]: edges){
             int u = edge[0];
             int v = edge[1];
-            
-            graph[v].add(u);
+            adj.get(v).add(u);
         }
 
         boolean vis[] = new boolean[V];
-        boolean[] pathVis = new boolean[V];
-        Stack<Integer> s = new Stack<>();
-        for(int i = 0; i<graph.length; i++){
+        boolean st[] = new boolean[V];
+        for(int i = 0; i<V; i++){
             if(!vis[i]){
-                 if (dfsUtil(graph, vis, pathVis, s, i)) {
-                    return new int[0]; 
-                }
+                if(isCycle(adj, i, vis, st)) return new int[]{};
             }
         }
-        
-        int ans[] = new int[V];
-       int idx = 0;
-        while (!s.isEmpty()) {
-            ans[idx++] = s.pop();
+
+        vis = new boolean[V];
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0; i<V; i++){
+            if(!vis[i]){
+                topSort(adj, i, vis, s);
+            }
         }
-        
+
+        int[] ans = new int[V];
+        int i = 0;
+        while(!s.isEmpty()){
+            ans[i] = s.pop();
+            i++;
+        }
         return ans;
     }
 }
