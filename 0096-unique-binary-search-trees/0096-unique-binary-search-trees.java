11b5250edcb68b8ad40b0d185bc91c77f90e1ed6
Divyasharma11245 <divyasharma11245@gmail.com>
2026-07-04 14:37:02 +0530

    Time: 0 ms (100%), Space: 42 MB (48.15%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -3,11 +3,11 @@ class Solution {
         int dp[] = new int[n+1];
         dp[0] = 1;
         dp[1] = 1;
-        for(int i =2; i<=n; i++){
-            for(int j = 0; j<=i-1; j++){
-                dp[i] += dp[j]*dp[i-j-1];
+        for(int i = 2; i<=n; i++){
+            for(int j = 0; j<i; j++){
+                dp[i]+=dp[j]*dp[i-j-1];
             }
         }
-        return dp[n]; 
+        return dp[n];
     }
 }
