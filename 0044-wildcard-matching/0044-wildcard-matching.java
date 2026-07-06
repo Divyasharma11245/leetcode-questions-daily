e9c98de8772d63fb21fe5702d59b274e430771d8
Divyasharma11245 <divyasharma11245@gmail.com>
2026-07-07 04:32:15 +0530

    Time: 54 ms (25.41%), Space: 47.3 MB (64.76%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -4,24 +4,18 @@ class Solution {
         int n = p.length();
         boolean dp[][] = new boolean[m+1][n+1];
         dp[0][0] = true;
-        for(int i =1; i<=m; i++){
+        for(int i = 1; i<=m; i++){
             dp[i][0] = false;
         }
         for(int j = 1; j<=n; j++){
-            if(p.charAt(j-1)=='*'){
-                dp[0][j] = dp[0][j-1];
+                if(p.charAt(j-1)=='*') dp[0][j] = dp[0][j-1];
             }
-        }
 
-        for(int i =1; i<=m; i++){
+        for(int i = 1; i<=m; i++){
             for(int j = 1; j<=n; j++){
-                if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }else if(p.charAt(j-1)=='*'){
-                    dp[i][j] = dp[i-1][j]||dp[i][j-1];
-                }else{
-                    dp[i][j] = false;
-                }
+                if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='?') dp[i][j] = dp[i-1][j-1];
+                else if(p.charAt(j-1)=='*') dp[i][j] = dp[i-1][j]||dp[i][j-1];
+                else dp[i][j] = false;
             }
         }
         return dp[m][n];
