ccc1031237ac1418c39ff16fa49685982d192977
Divyasharma11245 <divyasharma11245@gmail.com>
2026-07-09 12:33:53 +0530

    Time: 1 ms (87.47%), Space: 44.4 MB (20.68%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,16 +1,14 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         int n = nums.length;
+        int subset = 1<<n;
         List<List<Integer>> ans = new ArrayList<>();
-        int subsets = 1<<n; //2raised to power n
-        for(int i = 0; i<subsets; i++){
-            List<Integer> inner = new ArrayList<>();
-            for(int j = 0; j<n; j++){ //check if ith bit is set or not
-                if((i&(1<<j))!=0){
-                    inner.add(nums[j]);
-                }
+        for(int i = 0; i<=subset-1; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j<n; j++){
+                if((i&1<<j)==0) list.add(nums[j]);
             }
-            ans.add(inner);
+            ans.add(list);
         }
         return ans;
     }
