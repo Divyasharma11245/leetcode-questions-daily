31a85868d74b018f6c806f39a428fb6394d57b16
Divyasharma11245 <divyasharma11245@gmail.com>
2026-08-05 07:38:34 +0530

    Time: 1954 ms (7.05%), Space: 43.5 MB (69.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,26 @@
 class Solution {
+    private boolean isPalindrome(int i, int j, String s){
+        if(i>=j){
+            return true;
+        }
+        if(s.charAt(i)==s.charAt(j)) {
+            return isPalindrome(i+1, j-1, s);}
+        return false;
+    }
     public String longestPalindrome(String s) {
         int n = s.length();
-        boolean dp[][] = new boolean[n][n];
-
-        int start=0;
-        int maxLength=1;
-        for(int i = n-1; i>=0; i--){
-            for(int j = i; j<n;j++){
-                if(s.charAt(i)==s.charAt(j)){
-                    if(j-i<=2){
-                        dp[i][j]=true;
-                    }else{
-                        dp[i][j] = dp[i+1][j-1];
+        int maxLength = -1;
+        int st = -1;
+        for(int i = 0; i<n; i++){
+            for(int j = i; j<n; j++){
+                if(isPalindrome(i, j, s)){
+                   if(j - i + 1 > maxLength){
+                    maxLength = j - i + 1;
+                    st = i;
                     }
-                }
-                if(dp[i][j]&&j-i+1>maxLength){
-                    start=i;
-                    maxLength=j-i+1;
                 }
             }
         }
-        return s.substring(start, start+maxLength);
+        return s.substring(st, st+maxLength);
     }
 }
