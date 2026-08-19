955bff9a2ad4f39f8c9cee73c4c74a154df874fa
Divyasharma11245 <divyasharma11245@gmail.com>
2026-08-19 08:39:59 +0530

    Time: 7 ms (6.12%), Space: 46.1 MB (5.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,20 +1,37 @@
 class Solution {
-    private void generate(int n, List<String> list, int open, int close, String curr){
-        if(curr.length()==2*n){
-            list.add(curr);
+    List<String> list = new ArrayList<>();
+    private boolean isValid(String str){
+        int count = 0;
+        for(int i = 0; i<str.length(); i++){
+            if(str.charAt(i)=='(') count++;
+            else if(str.charAt(i)==')') count--;
+            if (count < 0)
+            return false;
+        }
+        if(count==0) return true;
+        return false;
+    }
+    private void solve(StringBuilder curr, int n){
+        if(curr.length()==n*2){
+            String str = curr.toString();
+            if(isValid(str)){
+                list.add(str);
+
+            }
             return;
         }
-        if(open<n){
-            generate(n, list, open+1, close, curr+'(');
-        }
-        if(close<open){
-            generate(n, list, open, close+1, curr+')');
-        }
+
+        curr.append('(');
+        solve(curr, n);
+        curr.deleteCharAt(curr.length() - 1);
+
+        curr.append(')');
+        solve(curr, n);
+        curr.deleteCharAt(curr.length() - 1);
     }
     public List<String> generateParenthesis(int n) {
-        ArrayList<String> list = new ArrayList<>();
-        String curr = "";
-        generate(n, list, 0, 0, curr);
+        StringBuilder str = new StringBuilder();
+        solve(str, n);
         return list;
     }
 }
