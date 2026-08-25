9c3bf646158e68ce68a2e86c3d9a96d94df14ab7
Divyasharma11245 <divyasharma11245@gmail.com>
2026-08-25 10:50:32 +0530

    Time: 198 ms (15.69%), Space: 43 MB (43.6%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,21 +1,16 @@
 class Solution {
-    private boolean find(char[][] board, int i, int j, int idx, String word){
-        int m = board.length;
-        int n = board[0].length;
-        if(idx==word.length()){
-            return true;
-        }
-        if(i<0||j<0||i>=m||j>=n||board[i][j]!=word.charAt(idx)||board[i][j]=='$'){
-            return false;
-        }
+    int[][] directions = {{0,1}, {0,-1},{1,0},{-1,0}};
+    private boolean find(char[][] board, String word, int i, int j, int idx){
+        if(idx>=word.length()) return true;
+        if(i>=board.length||j>=board[0].length||i<0||j<0||board[i][j]=='$'||board[i][j]!=word.charAt(idx)) return false;
 
         char temp = board[i][j];
         board[i][j] = '$';
-        int dir[][] = {{0,1}, {1,0}, {-1, 0}, {0, -1}};
-        for(int[] d:dir){
-            int i_ = i+d[0];
-            int j_ = j+d[1];
-            if(find(board, i_, j_, idx+1, word)) return true;
+        for(int[] dir: directions){
+            int new_i = i+dir[0];
+            int new_j = j+dir[1];
+            if(find(board, word, new_i, new_j, idx+1)) return true;
+            
         }
         board[i][j] = temp;
         return false;
@@ -23,9 +18,9 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
-        for(int i =0; i<m; i++){
+        for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if((board[i][j] == word.charAt(0)&&find(board, i, j, 0, word))) return true;
+                if(board[i][j]==word.charAt(0)&&find(board, word, i, j, 0)) return true;
             }
         }
         return false;
