3366acd1d5a9b8d7958e5bd882c2969abeaf3064
Divyasharma11245 <divyasharma11245@gmail.com>
2026-09-06 07:49:17 +0530

    Time: 112 ms (11.33%), Space: 76.1 MB (91.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,38 +1,38 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int leftSmaller[] = new int[n];
-        int rightSmaller[] = new int[n];
+    private int[] findPrevSmaller(int[] arr){
+        int n = arr.length;
         Stack<Integer> st = new Stack<>();
+        int ans[] = new int[n];
         for(int i = 0; i<n; i++){
-            while(!st.isEmpty()&&heights[st.peek()]>=heights[i]){
+            while(!st.isEmpty()&&arr[st.peek()]>=arr[i]){
                 st.pop();
             }
-            if(st.isEmpty()){
-                leftSmaller[i] = -1;
-            }else{
-                leftSmaller[i] = st.peek();
-            }
+            ans[i] = st.isEmpty()?-1:st.peek();
             st.push(i);
         }
-        st = new Stack<>();
-        for(int i = n-1; i>=0; i--){
-            while(!st.isEmpty()&&heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                rightSmaller[i] = n;
-            }else{
-               rightSmaller[i] = st.peek();
-            }
-            st.push(i);
-        }
+        return ans;
+    }
 
+        private int[] findNextSmaller(int[] arr){
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int ans[] = new int[n];
+        for(int i = n-1; i>=0; i--){
+            while(!st.isEmpty()&&arr[st.peek()]>=arr[i]){
+                st.pop();
+            }
+            ans[i] = st.isEmpty()?n:st.peek();
+            st.push(i);
+        }
+        return ans;
+    }
+    public int largestRectangleArea(int[] heights) {
+        int[] nextSmaller = findNextSmaller(heights);
+        int[] prevSmaller = findPrevSmaller(heights);
+        int n = heights.length;
         int ans = 0;
-        for(int i = 0; i<n; i++){
-            int width = rightSmaller[i]-leftSmaller[i]-1;
-            int currArea = heights[i]*width;
-            ans = Math.max(ans, currArea);
+        for(int i=0; i<n; i++){
+            ans = Math.max(ans, heights[i]*(nextSmaller[i]-prevSmaller[i]-1));
         }
         return ans;
     }
