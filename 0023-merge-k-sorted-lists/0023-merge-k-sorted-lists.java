d6d2952261939f0c4f3f1152e8995bc118f042c6
Divyasharma11245 <divyasharma11245@gmail.com>
2026-09-15 14:25:46 +0530

    Time: 162 ms (5.08%), Space: 46.7 MB (64.82%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,35 +9,38 @@
  * }
  */
 class Solution {
-     private ListNode merge(ListNode list1, ListNode list2) {
+    private ListNode merge(ListNode head, ListNode nextList){
+        ListNode temp1 = head;
+        ListNode temp2 = nextList;
         ListNode mergedLL = new ListNode(-1);
         ListNode temp = mergedLL;
-        while(list1!=null&&list2!=null){
-            if(list1.val<=list2.val){
-                temp.next = list1;
-                list1 = list1.next;
+        while(temp1!=null&&temp2!=null){
+            if(temp1.val<=temp2.val){
+                temp.next = temp1;
+                temp1 = temp1.next;
                 temp = temp.next;
             }else{
-                temp.next = list2;
-                list2 = list2.next;
+                temp.next = temp2;
+                temp2 = temp2.next;
                 temp = temp.next;
             }
         }
-            while(list1!=null){
-                temp.next = list1;
-                list1 = list1.next;
-                temp = temp.next;
-            }
-            while(list2!=null){
-            temp.next = list2;
-                list2 = list2.next;
-                temp = temp.next;
+        while(temp1!=null){
+            temp.next = temp1;
+            temp1 = temp1.next;
+            temp = temp.next;
+        }
+        while(temp2!=null){
+            temp.next = temp2;
+            temp2 = temp2.next;
+            temp = temp.next;
         }
         return mergedLL.next;
     }
     public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
         ListNode newMerged = null;
-        for(int i = 0; i<lists.length; i++){
+        for(int i = 0; i<n; i++){
             newMerged = merge(newMerged, lists[i]);
         }
         return newMerged;
