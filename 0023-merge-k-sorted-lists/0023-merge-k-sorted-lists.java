e68c40dc4a3dd1f87b6880f651626c0d60187905
Divyasharma11245 <divyasharma11245@gmail.com>
2026-09-16 11:30:13 +0530

    Time: 5 ms (40.72%), Space: 46.6 MB (79.74%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,40 +9,22 @@
  * }
  */
 class Solution {
-    private ListNode merge(ListNode head, ListNode nextList){
-        ListNode temp1 = head;
-        ListNode temp2 = nextList;
-        ListNode mergedLL = new ListNode(-1);
-        ListNode temp = mergedLL;
-        while(temp1!=null&&temp2!=null){
-            if(temp1.val<=temp2.val){
-                temp.next = temp1;
-                temp1 = temp1.next;
-                temp = temp.next;
-            }else{
-                temp.next = temp2;
-                temp2 = temp2.next;
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        ListNode dummy = new ListNode(-1);
+        ListNode k = dummy;
+        for(ListNode list : lists){
+            ListNode temp = list;
+            while(temp!=null){
+                pq.add(temp.val);
                 temp = temp.next;
             }
         }
-        while(temp1!=null){
-            temp.next = temp1;
-            temp1 = temp1.next;
-            temp = temp.next;
+        while(pq.size()>0){
+            ListNode node = new ListNode(pq.remove());
+            k.next = node;
+            k = k.next;
         }
-        while(temp2!=null){
-            temp.next = temp2;
-            temp2 = temp2.next;
-            temp = temp.next;
-        }
-        return mergedLL.next;
-    }
-    public ListNode mergeKLists(ListNode[] lists) {
-        int n = lists.length;
-        ListNode newMerged = null;
-        for(int i = 0; i<n; i++){
-            newMerged = merge(newMerged, lists[i]);
-        }
-        return newMerged;
+        return dummy.next;
     }
 }
